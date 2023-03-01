69df53190c1aa6c2b32649556fc91fa1bed3d33d
Ada <the.nostra.tymus@gmail.com>
2023-03-01 07:05:14 -0500

    updated pypi

--- build/lib/satnogs_webscraper/request_utils.py
+++ build/lib/satnogs_webscraper/request_utils.py
@@ -23,7 +23,7 @@ def get_request(url):
             sleep_amount = count ** 2
             write_log(url, -1, f"Exception:{e}")
             time.sleep(sleep_amount)
-            if count == 2:
+            if count == 10:
                 return None
         count += 1
 
