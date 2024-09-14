c3838ed28bf373dcdc7f37cbfb6df6c9bf1a9781
Ada <the.nostra.tymus@gmail.com>
2024-09-14 18:29:14 -0400

    renamed the demod task list

--- satnogs_webscraper/observation_scraper.py
+++ satnogs_webscraper/observation_scraper.py
@@ -97,16 +97,12 @@ class ObservationScraper:
             template['Observation_id'] = observation
 
             template['demods'] = []
+            demod_tasks = []
 
-            # multiprocess data_fetch.
-            # for data_a in observation_web_page.find_all("a", class_='data-link'):
-            #     template['demods'].append(self.fetch_demod(data_a))
-
-            demods = []
             for data_a in observation_web_page.find_all("a", class_='data-link'):
-                demods.append(dask.delayed(self.fetch_demod)(data_a))
+                demod_tasks.append(dask.delayed(self.fetch_demod)(data_a))
 
-            template['demods'] = dask.compute(*[demods])[0]
+            template['demods'] = dask.compute(*[demod_tasks])[0]
             with open(os.path.join(cnst.directories['observations'], f"{observation}.json"), 'w') as obs_out:
                 json.dump(template, obs_out)
 
