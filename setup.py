48ac0482727c1c296fed4b4a85ec646209e614c3
Ada <the.nostra.tymus@gmail.com>
2024-09-14 22:56:34 -0400

    updated the version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-VERSION = '1.4.2'
+VERSION = '1.4.3'
 DESCRIPTION = 'Scrape SATNOGS data into a pandas dataframe'
 LONG_DESCRIPTION = 'A webscraper to pull multiple satnogs observations into a single pandas dataframe'
 
